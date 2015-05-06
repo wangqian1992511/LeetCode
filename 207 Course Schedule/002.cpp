2fc8f3059a266cb2794b658654d33febc5f320f2
Qian Wang <wangqian1992511@gmail.com>
2015-05-07 02:26:15 +0800

    Update 002.cpp

--- 207 Course Schedule/002.cpp
+++ 207 Course Schedule/002.cpp
@@ -3,22 +3,18 @@ public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int> *adj = new vector<int> [numCourses];
         int *deg = new int [numCourses];
-        bool *vis = new bool [numCourses];
         fill(deg, deg + numCourses, 0);
-        fill(vis, vis + numCourses, 0);
-        
-        int nEdge = prerequisites.size();
-        for (int i = 0; i < nEdge; i++) {
-            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
-            deg[prerequisites[i][1]]++;
+
+        for (int i = prerequisites.size() - 1; i >= 0; i--) {
+            vector<int> e = prerequisites[i];
+            adj[e[0]].push_back(e[1]);
+            deg[e[1]]++;
         }
         
         queue<int> zero;
         for (int i = numCourses - 1; i >= 0; i--) 
-            if (!deg[i]) {
+            if (!deg[i]) 
                 zero.push(i);
-                vis[i] = true;
-            }
         
         int d = 0, u, v, n; 
         while (!zero.empty()) {
@@ -28,8 +24,7 @@ public:
             d++;
             for (int j = 0; j < n; j++) {
                 v = adj[u][j];
-                deg[v]--;
-                if (!deg[v] && !vis[v])
+                if (! --deg[v]) 
                     zero.push(v);
             }
         }
