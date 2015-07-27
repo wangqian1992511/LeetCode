29b46051965a4717eb39194c102f5c3479b4cd34
Qian Wang <wangqian1992511@gmail.com>
2015-07-28 04:03:39 +0800

    132

---
+++ 132 Palindrome Partitioning II/001.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minCut(string s) {
+        this->s = s;
+        n = s.size();
+        build();
+        dp[0] = 0;
+        for (int i = 1; i < n; i++) {
+            if (f[0][i]) {
+                dp[i] = 0;
+                continue;
+            }
+            dp[i] = INT_MAX;
+            for (int j = 0; j < i; j++)
+                if (f[j + 1][i])
+                    dp[i] = min(dp[i], dp[j] + 1);
+        }
+        return dp[n-1];
+    }
+private:
+    bool **f;
+    int *dp;
+    string s;
+    int n;
+    void build() {
+        f = new bool* [n];
+        dp = new int [n];
+        for (int i = 0; i < n; i++) {
+            f[i] = new bool [n];
+            f[i][i] = true;
+            f[i][i+1] = (i != n-1) && (s[i] == s[i+1]);
+        }
+        for (int len = 3; len <= n; len++)
+            for (int i = 0; i <= n - len; i++)
+                f[i][i+len-1] = f[i+1][i+len-2] && (s[i] == s[i+len-1]);
+    }    
+};
---
+++ 132 Palindrome Partitioning II/readme.txt
@@ -0,0 +1,6 @@
+001.cpp (24ms):
+
+A DP problem with the transfer function:
+
+f[i] = f[j] + 1 (st[j+1...i] is palindrome)
+
