96c7efe728bf17171248f9c3c44e7cac06d5459a
Qian Wang <wangqian1992511@gmail.com>
2015-07-28 15:34:01 +0800

    132

--- 132 Palindrome Partitioning II/001.cpp
+++ 132 Palindrome Partitioning II/001.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
     int minCut(string s) {
-        this->s = s;
-        n = s.size();
-        build();
+        int n = s.size();
+        bool **f = new bool* [n];
+        int *dp = new int [n];
+        
+        for (int i = 0; i < n; i++) {
+            f[i] = new bool [n];
+            f[i][i] = true;
+            f[i][i+1] = (i != n-1) && (s[i] == s[i+1]);
+        }
+        for (int len = 3; len <= n; len++)
+            for (int i = 0; i <= n - len; i++)
+                f[i][i+len-1] = f[i+1][i+len-2] && (s[i] == s[i+len-1]);
+                
         dp[0] = 0;
         for (int i = 1; i < n; i++) {
             if (f[0][i]) {
@@ -15,23 +25,7 @@ public:
                 if (f[j + 1][i])
                     dp[i] = min(dp[i], dp[j] + 1);
         }
+        
         return dp[n-1];
     }
-private:
-    bool **f;
-    int *dp;
-    string s;
-    int n;
-    void build() {
-        f = new bool* [n];
-        dp = new int [n];
-        for (int i = 0; i < n; i++) {
-            f[i] = new bool [n];
-            f[i][i] = true;
-            f[i][i+1] = (i != n-1) && (s[i] == s[i+1]);
-        }
-        for (int len = 3; len <= n; len++)
-            for (int i = 0; i <= n - len; i++)
-                f[i][i+len-1] = f[i+1][i+len-2] && (s[i] == s[i+len-1]);
-    }    
 };
