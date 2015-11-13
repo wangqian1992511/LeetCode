89adf7b84c98f5d81b3d042b393fb94a2a30a65e
Qian Wang <wangqian1992511@gmail.com>
2015-11-13 09:18:50 -0800

    cpp

--- 261 Graph Valid Tree/001.cpp
+++ 261 Graph Valid Tree/001.cpp
@@ -2,26 +2,35 @@ class Solution {
 public:
     bool validTree(int n, vector<pair<int, int>>& edges) {
         if (edges.size() != n-1) return false;
-        for (int i = 0; i < n; i++) father.push_back(i);
+        father = new int [n];
+        rank = new int [n];
+        memset(rank, 0, n * sizeof(int));
+        for (int i = 0; i < n; i++)
+            father[i] = i;
         for (auto e = edges.begin(); e != edges.end(); e++) {
-            int u = e->first;
-            int v = e->second;
-            int fu = getFather(u);
-            int fv = getFather(v);
+            int fu = getFather(e->first);
+            int fv = getFather(e->second);
             if (fu == fv) return false;
             unionIt(fu, fv);
         }
         return true;
     }
 private:
-    vector<int> father;
+    int *father;
+    int *rank;
     int getFather (int x) {
-        if (father[x] == x) return x;
-        int f = getFather(father[x]);
-        father[x] = f;
-        return f;
+        if (father[x] != x)
+            father[x] = getFather(father[x]);
+        return father[x];
     }
     void unionIt(int a, int b) {
-        father[a] = b;
+        if (rank[a] < rank[b])
+            father[a] = b;
+        else if (rank[b] < rank[a])
+            father[b] = a;
+        else {
+            father[b] = a;
+            rank[a]++;
+        }
     }
 };
